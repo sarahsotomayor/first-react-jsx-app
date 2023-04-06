c213a0ac2fd03c9fde1c15edb3c7d534846beffb
Sarah <116047642+ellaeslaflaca@users.noreply.github.com>
2023-04-05 19:51:08 -0600

    first react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Create a template using Bootstrap Sass</li>
+        <li>Finish my portfolio</li>
+        <li>Play Minecraft</li>
+      </ul>
     </div>
   );
 }
